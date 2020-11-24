d3320c90e14e643e4b6e88cebc3bb218dca01c74
m-rothe <marcel@henkerservices.de>
2020-11-24 10:24:05 +0100

    Bugfix: Readded missing import

--- ivod_platform/platformAPI/views.py
+++ ivod_platform/platformAPI/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpRequest, HttpResponseRedirect, HttpResponseForbidden
 from django.contrib.auth.models import User, Group
 from .models import EnhancedUser, EnhancedGroup, Datasource, Chart
